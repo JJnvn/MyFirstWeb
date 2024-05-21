98ba411fc866d89567c61f553858aa195eac92f3
JJnvn <123181209+JJnvn@users.noreply.github.com>
2024-05-21 18:11:50 +0700

    install nodemon

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-// import http to run server
-const http = require('http')
+const express = require('express')
+const app = express()
 
-// set default host, port
-// this equal to localhost:8000 (localhost is a network interface that allows communication between processes running on the same device)
-// network interface == bridge that connect device to internet
-const host = '127.0.0.1' 
+// const host = 'localhost'
 const port = 8000
 
-// กำหนดค่าเริ่มต้นของ server เมื่อเปิดหน้าเว็บที่ localhost:8000 ขึ้นมา
-const requestListener = function (req, res) {
-  res.writeHead(200)
-  res.end("My first server!")
-}
+// path = '/'
+app.get('/', (req, res) => {
+  let user = {
+    firstname: 'test',
+    lastname: 'lastn',
+    age: 14
+  }
+  // res.send('Hello world from port ' + port)
+  res.json(user)
+})
 
-// run server
-const server = http.createServer(requestListener)
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)
+// start the server
+app.listen(port, (req, res) => {
+  console.log(`http server run at ${port}`)
 })
