82ff1fec939091de8b965062fa98f7a2c53817d0
JJnvn <123181209+JJnvn@users.noreply.github.com>
2024-05-26 11:24:07 +0700

    finish mock API

--- server/index.js
+++ server/index.js
@@ -1,18 +1,82 @@
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
+
+app.use(bodyparser.json())
+
+const users = []
+let counter = 1
 
 // const host = 'localhost'
 const port = 8000
 
 // path = '/'
-app.get('/', (req, res) => {
-  let user = {
-    firstname: 'test',
-    lastname: 'lastn',
-    age: 14
+app.get('/users', (req, res) => {
+  res.send(users)
+})
+
+app.post('/user', (req, res) => {
+  let user = req.body
+  user.id = counter
+  counter += 1
+  users.push(user)
+  res.json({
+    message: 'add ok',
+    user: user
+  })
+})
+
+app.put('/user/:id', (req, res) => {
+  let id = req.params.id
+  let updateUser = req.body
+
+  let selectedIndex = users.findIndex(user => user.id == id)
+  // users[selectedIndex] = updateUser // this will delete the id key
+  users[selectedIndex].firstName = updateUser.firstName || users[selectedIndex].firstName
+  users[selectedIndex].lastName = updateUser.lastName || users[selectedIndex].lastNametName
+  // res.send(selectedIndex + '')
+  res.json({
+    message: 'update user complete',
+    data: {
+      user: updateUser,
+      indexUpdate: selectedIndex
+    }
+  })
+})
+
+app.patch('/user/:id', (req, res) => {
+  let id = req.params.id
+  let updateUser = req.body
+
+  let selectedIndex = users.findIndex(user => user.id == id)
+  if(updateUser.firstName){
+    users[selectedIndex].firstName = updateUser.firstName
   }
-  // res.send('Hello world from port ' + port)
-  res.json(user)
+  if(updateUser.lastNamestName){
+    users[selectedIndex].lastName = updateUser.lastNamestName
+  }
+
+  res.json({
+    message: 'patch user complete',
+    data: {
+      user: updateUser,
+      indexUpdate: selectedIndex
+    }
+  })
+})
+
+app.delete('/user/:id', (req, res) => {
+  let id = req.params.id
+
+  let selectedIndex = users.findIndex(user => user.id == id)
+
+  // delete users[selectedIndex]
+  users.splice(selectedIndex, 1)
+
+  res.json({
+    message: 'delete complete',
+    indexDeleted: selectedIndex
+  })
 })
 
 // start the server
