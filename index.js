4506201f7bd465167bb2a6100bfd733f1ef74b0a
JJnvn <123181209+JJnvn@users.noreply.github.com>
2024-05-30 15:45:15 +0700

    connect frontend to API

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-function submitData(){
-    let firstnameDOM = document.querySelector('input[name=firstname]')
-    let lastnameDOM = document.querySelector('input[name=lastname]')
-    let ageDOM = document.querySelector('input[name=age]')
+const submitData = async () => {
+  let firstnameDOM = document.querySelector("input[name=firstname]");
+  let lastnameDOM = document.querySelector("input[name=lastname]");
+  let ageDOM = document.querySelector("input[name=age]");
 
-    let genderDOM = document.querySelector('input[name=gender]:checked')
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked')
+  let genderDOM = document.querySelector("input[name=gender]:checked");
+  let interestDOMs = document.querySelectorAll("input[name=interest]:checked");
 
-    let descriptionDOM = document.querySelector('textarea[name=description]')
+  let descriptionDOM = document.querySelector("textarea[name=description]");
 
-    let interestDOM = ""
-    for(let i = 0; i < interestDOMs.length; i++){
-        interestDOM += interestDOMs[i].value
-        if(i != interestDOMs.length - 1){
-            interestDOM += ', '
-        }
+  let interestDOM = "";
+  for (let i = 0; i < interestDOMs.length; i++) {
+    interestDOM += interestDOMs[i].value;
+    if (i != interestDOMs.length - 1) {
+      interestDOM += ", ";
     }
-    let userData = {
-        firstname: firstnameDOM.value,
-        lastname: lastnameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        interest: interestDOM,
-        description: descriptionDOM.value
-
-    }
-    console.log('submit data', userData)
-}
+  }
+  let userData = {
+    firstname: firstnameDOM.value,
+    lastname: lastnameDOM.value,
+    age: ageDOM.value,
+    gender: genderDOM.value,
+    interest: interestDOM,
+    description: descriptionDOM.value,
+  };
+  console.log("submit data", userData);
+  const response = await axios.post("http://localhost:8000/users/", userData);
+  console.log("response", response.data);
+};
